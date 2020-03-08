7b21331e61d141072238f34ee43f7aaee2f3440a
Daniel Brito <dannyb829@gmail.com>
2020-03-08 04:37:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,20 @@
 class Person
 
+def name=(person_name)
+  person_name = @name
+end
 
+def name
+  @name
+end
 
+def job=(job_title)
+  job_title = @job
+end
 
-
-
-
-
-
-
-
+def job
+  @job
+end
 
 
 end
